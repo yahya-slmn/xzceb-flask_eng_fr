1be53a9c2f5b3f0241a214ab88cd0ec2a22fbb06
yahya-slmn <miazfield@gmail.com>
2023-05-20 10:45:55 -0400

    Temporary changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,26 +1,27 @@
 import unittest
-
-from translator import frenchToEnglish, englishToFrench
+from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
-    def test_englishToFrench(self):
+    def test_english_to_french(self):
         # Test translation of "Hello" to French
-        result = englishToFrench("Hello")
+        result = english_to_french("Hello")
         self.assertEqual(result, "Bonjour")
 
         # Test translation of empty string
-        result = englishToFrench("")
+        result = english_to_french("")
         self.assertEqual(result, "")
 
-    def test_frenchToEnglish(self):
+
+    def test_french_to_english(self):
         # Test translation of "Bonjour" to English
-        result = frenchToEnglish("Bonjour")
+        result = french_to_english("Bonjour")
         self.assertEqual(result, "Hello")
 
         # Test translation of empty string
-        result = frenchToEnglish("")
+        result = french_to_english("")
         self.assertEqual(result, "")
-        
+
+
 if __name__ == '__main__':
     unittest.main()
 
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,6 @@
-
-import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,28 +16,36 @@ translator = LanguageTranslatorV3(
 )
 
 # Set the service URL (e.g. "https://api.us-south.language-translator.watson.cloud.ibm.com")
-translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/236013d9-44fd-47c8-a74e-3658973fd794')
+URL = 'https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/236013d9-44fd-47c8-a74e-3658973fd794'
+translator.set_service_url(URL)
 
-def englishToFrench(englishText):
+"""
+This module provides functions for translating text between English and French.
+"""
+def english_to_french(english_text):
     # Translate English to French
+    if not english_text:
+        return ""
     translation = translator.translate(
-        text=englishText,
+        text=english_text,
         source='en',
         target='fr'
     ).get_result()
 
     # Extract and return the translated text
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
     # Translate French to English
+    if not french_text:
+        return ""
     translation = translator.translate(
-        text=frenchText,
+        text=french_text,
         source='fr',
         target='en'
     ).get_result()
 
     # Extract and return the translated text
-    englishText = translation['translations'][0]['translation']
-    return englishText 
+    english_text = translation['translations'][0]['translation']
+    return english_text
